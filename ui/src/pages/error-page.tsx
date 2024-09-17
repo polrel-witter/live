e0d11d5e2edcf4b8c3a4815a203a171262fc3459
widmes-hassen <widmes-hassen@pm.me>
2024-09-17 18:22:22 +0200

    started implementing routes;

---
+++ ui/src/pages/error-page.tsx
@@ -0,0 +1,28 @@
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+
+export function ErrorPage() {
+  const error = useRouteError();
+  let errorMessage: string;
+
+  if (isRouteErrorResponse(error)) {
+    // error is type `ErrorResponse`
+    errorMessage = error.data.error?.message || error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === 'string') {
+    errorMessage = error;
+  } else {
+    console.error(error);
+    errorMessage = 'Unknown error';
+  }
+
+  return (
+    <div>
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{errorMessage}</i>
+      </p>
+    </div>
+  );
+}
---
+++ ui/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export function Index() {
+  return (
+    <div className="max-w-2lg space-y-6 py-20 text-center">
+      <h1 className="text-3xl italic">*list of events*</h1>
+    </div>
+  )
+}
