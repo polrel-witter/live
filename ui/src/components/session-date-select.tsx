ffea01504525590e847c7d7ab1490419c03aac2b
widmes-hassen <widmes-hassen@pm.me>
2024-09-24 11:20:38 +0200

    make schedule page default to first day in event;

--- ui/src/components/session-date-select.tsx
+++ ui/src/components/session-date-select.tsx
@@ -11,8 +11,11 @@ export function SessionDateSelect(props: { sessionDates: Date[], changeDate: (s:
     <Select
       onValueChange={(s: string) => props.changeDate(new Date(Date.parse(s)))
       }>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select Date" />
+      <SelectTrigger  className="w-[180px]">
+        <SelectValue
+          defaultValue={props.currentDate ? props.currentDate.toISOString() : "Select Date"}
+          placeholder={props.currentDate.toDateString() || "Select Date"}
+        />
       </SelectTrigger>
       <SelectContent>
         {props.sessionDates.map(date => <SelectItem key={date.toISOString()} value={date.toISOString()}>{date.toDateString()}</SelectItem>)}
