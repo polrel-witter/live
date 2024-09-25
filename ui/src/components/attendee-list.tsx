291d2b0ce75b251eb2308255c410353dbb507d0e
widmes-hassen <widmes-hassen@pm.me>
2024-09-25 12:32:43 +0200

    figured out transitions;

--- ui/src/components/attendee-list.tsx
+++ ui/src/components/attendee-list.tsx
@@ -8,33 +8,63 @@ import {
 } from "@/components/ui/card"
 
 import { Profile, Session } from "@/backend"
-import { defaultMaxListeners } from "events"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Handshake, Users, ChevronDown } from 'lucide-react'
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
 
-function formatSessionPanel(panel: string[]): string {
-  switch (panel.length) {
-    case 0:
-      return ""
-    case 1:
-      return `with ${panel[0]}`
-    case 2:
-      return `with ${panel[0]} and ${panel[1]}`
-    default:
-      return `with ${panel.slice(0, -1).join(", ")} and ${panel[panel.length - 1]}`
-  }
+function makeConnectionsCard() {
+  return (
+    <Card className="w-24 border rounded p-0.5">
+      {/*
+      * possile states:
+      * - matched: we show "matched" on green bg, or green check on green bg that says "matched" on hover 
+      * - we sent match request: we show "proposed match" on light blue bg,
+      *   or light blue check on green bg that says "asked match" on hover;
+      *   we also add red cross icon to annull match
+      * - we did nothing: we show blank bg with match icon or "ask to match" text
+      */}
+      <CardHeader className="text-xs space-0 p-0.5 text-center"> connections </CardHeader>
+      <CardContent className="p-0.5">
+        <div className="flex justify-around">
+          <span className="h-4 w-4 bg-red-600"> </span>
+          <span className="h-4 w-4 bg-red-600"> </span>
+        </div>
+      </CardContent>
+    </Card>
+  )
 }
 
 function makeAttendeeMarkup(profile: Profile) {
+  const [b, setB] = useState(false)
   return (
-    <li key={window.crypto.randomUUID()}>
-      <Card>
-        <CardContent className="px-6 py-3 content-center">
-          <div className="flex">
+    <li key={profile.patp}>
+      <Card >
+        <CardContent
+          className={
+            "px-6 py-3 content-center"}>
+          <div className="flex gap-4 gap-x-10 items-center">
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-            <span className="justify-self-end self-middle">{profile.patp}</span>
+            <div className="flex flex-col items-center gap-1">
+              <p className="text-sm">{profile.patp}</p>
+              <Button className="h-4 w-4 p-0 border rounded-2xl bg-transparent">
+                <ChevronDown className="h-3 w-3 text-gray-400" />
+              </Button>
+            </div>
+            <Button onClick={() => setB((prev) => !prev)} className="w-10 h-10 p-0 border rounded-full bg-transparent">
+              <Users className="h-4 w-4 text-gray-400" />
+              {/* <Handshake className="h-4 w-4 opacity-50"/> */}
+              {/* <span className="text-2xl opacity-50">🤝</span> */}
+            </Button>
+          </div>
+          <div
+            className={"overflow-hidden transition-[max-height] duration-500 ease-in-out "
+              + (b ? "max-h-[300px]" : "max-h-0")}
+          >
+            {makeConnectionsCard()}
           </div>
         </CardContent>
       </Card>
