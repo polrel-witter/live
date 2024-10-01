03aca62fff47513fb1acf330db556d4d013a40ea
widmes-hassen <widmes-hassen@pm.me>
2024-10-01 11:34:45 +0200

    add profile dialog;

--- ui/src/components/navbar.tsx
+++ ui/src/components/navbar.tsx
@@ -1,10 +1,32 @@
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, } from "@/components/ui/navigation-menu"
 import { Link } from "react-router-dom"
+import ProfileDialog from "./profile-dialog"
+import { useState } from "react"
+import { Button } from "./ui/button"
+import { Edit, User } from "lucide-react"
+import { flipBoolean } from "@/lib/utils"
+import { Backend, EditableProfileFields, } from "@/backend"
 
-export default function NavBar(props: { eventName: string, host: string }) {
+export default function NavBar(props: { eventName: string, host: string, profile: EditableProfileFields, editProfileField: Backend["editProfileField"] }) {
+  const [openProfile, setOpenProfile] = useState(false)
+
   return (
     <NavigationMenu >
       <NavigationMenuList className="static">
+        <NavigationMenuItem className="fixed left-0">
+          <Button className="p-3 m-1 rounded-3xl">
+            <User
+              onClick={() => { setOpenProfile(flipBoolean) }}
+              className="w-4 h-4 text-white"
+            />
+          </Button>
+          <ProfileDialog
+            onOpenChange={setOpenProfile}
+            open={openProfile}
+            profileFields={props.profile}
+            editProfileField={props.editProfileField}
+          />
+        </NavigationMenuItem>
         <NavigationMenuItem className="grow text-center"> {props.eventName || "event"} </NavigationMenuItem>
         <NavigationMenuItem className="fixed right-0">
           <NavigationMenuTrigger />
