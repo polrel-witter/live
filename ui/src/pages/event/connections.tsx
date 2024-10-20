dd1488c705a76781da8b28db363690d91053d723
widmes-hassen <widmes-hassen@pm.me>
2024-10-20 18:28:09 +0200

    wip connections page;
    - added new sizes to profile picture;

--- ui/src/pages/event/connections.tsx
+++ ui/src/pages/event/connections.tsx
@@ -2,13 +2,25 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel"
-import { MatchingCard } from "@/components/matching-card"
 import { useContext, useEffect, useState } from "react"
 import { EventContext } from "./context"
+import ProfilePicture from "@/components/profile-picture"
+import { Profile } from "@/backend"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import ProfileCard from "@/components/profile-card"
+import { SpinningButton } from "@/components/spinning-button"
+import { type CarouselApi } from "@/components/ui/carousel"
 
+
+type Props = {
+  profile: Profile
+}
+
+// TODO: wrap up:
+//       - hook up match function & scrollNext on spinning-button
+//       - hook up scrollnext on other button
+//       - done?
 const ConnectionsPage: React.FC = () => {
   const ctx = useContext(EventContext)
 
@@ -16,7 +28,21 @@ const ConnectionsPage: React.FC = () => {
     throw Error("context is null")
   }
 
+  const [api, setApi] = useState<CarouselApi>()
   const [width, setWidth] = useState<number>(window.innerWidth);
+  const [spinButton1, setSpinButton1] = useState<boolean>(false);
+  const [spinButton2, setSpinButton2] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!api) {
+      return
+    }
+
+    api.on("select", () => {
+      // Do something on select.
+    })
+  }, [api])
+
 
 
   const handleWindowSizeChange = () => { setWidth(window.innerWidth); }
@@ -31,26 +57,44 @@ const ConnectionsPage: React.FC = () => {
   const isMobile = width <= 768;
 
   return (
-    <div className="flex justify-center m-4 mt-20">
+    <div className="flex justify-center m-4 mt-5">
       <Carousel
-        className="w-full h-full mx-4 md:w-60 md:h-100 "
-
+        setApi={setApi}
+        className="w-80 h-full mx-4"
       >
-        <CarouselContent>
-          {ctx.profiles.map(profile =>
-            <CarouselItem key={profile.patp}>
-              <MatchingCard profile={profile} />
-            </CarouselItem>)}
+        <CarouselContent className="m-0">
+          {ctx.profiles.map(profile => {
+            const attendee = ctx.attendees.find((a) => a.patp === profile.patp)
+            return (
+              < CarouselItem key={profile.patp} className=" p-0" >
+                <div className="flex-grow justify-items-center">
+                  <div className="inline-flex w-full h-full justify-center">
+                    <ProfilePicture
+                      avatarUrl={profile?.avatar ?? undefined}
+                      size="xl"
+                      point={profile.patp} />
+                  </div>
+                  <ProfileCard
+                    profile={profile}
+                    showHeader
+                  />
+                  <div className="w-full flex justify-around">
+                    <SpinningButton
+                      onClick={() => { setSpinButton1(true); api?.scrollNext() }}
+                      spin={false}
+                    >
+                    </SpinningButton>
+                    <SpinningButton spin> skip</SpinningButton>
+                  </div>
+                </div>
+              </CarouselItem>)
+          }
+          )}
         </CarouselContent>
-        {
-          !isMobile
-            ? <div> <CarouselPrevious /> <CarouselNext /> </div>
-            : ''
-        }
       </Carousel>
 
 
-    </div>
+    </div >
   )
 }
 
