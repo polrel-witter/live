a93125459353f56f631f3676c87c9670fc5a30b3
widmes-hassen <widmes-hassen@pm.me>
2024-11-26 18:29:17 +0100

    centered & fixed offset, was bothering me;

--- ui/src/components/button-toggle.tsx
+++ ui/src/components/button-toggle.tsx
@@ -12,11 +12,11 @@ const ButtonToggle = (props: Props) => {
       onPressedChange={props.onPressedChange}
       pressed={props.pressed}
     >
-      <div className="w-8 h-5 bg-stone-200 relative">
+      <div className="inline-flex items-center w-8 h-5 bg-stone-200 relative">
         <span className={cn([
           "transition-colors transition-[right]",
           "w-4 h-4 bg-black absolute rounded-lg block",
-          { "right-[10px] bg-stone-400": !props.pressed },
+          { "right-[16px] bg-stone-400": !props.pressed },
           { "right-[0px] bg-emerald-400": props.pressed }
         ])} />
       </div>
