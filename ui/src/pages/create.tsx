dc32f4bdfc0c578b88022a9167e4e04e740cd0aa
sage <sa24@protonmail.com>
2024-12-02 06:14:28 -0500

    live: replaced timeline navbar
    now the navbar just has a back button; profile and create event buttons
    weren't necessary.
    
    i also moved the 'create event' text to the top to reduce spacing.

--- ui/src/pages/create.tsx
+++ ui/src/pages/create.tsx
@@ -1,7 +1,17 @@
 import { Backend } from "@/backend";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "@/globalContext";
+import { AppFrame } from "@/components/frame"
 import { CreateEventForm } from "@/components/forms/create-event";
+import { flipBoolean } from "@/lib/utils"
+
+import { X } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { BackButton } from "@/components/back-button"
+import { NavbarWithSlots } from "@/components/frame/navbar"
+import { FooterWithSlots } from "@/components/frame/footer"
+import { ConnectionStatusBar } from "@/components/connection-status"
+
 
 const CreatePage: React.FC<{ backend: Backend }> = ({ backend }) => {
   const globalContext = useContext(GlobalContext)
@@ -11,17 +21,44 @@ const CreatePage: React.FC<{ backend: Backend }> = ({ backend }) => {
     return
   }
 
-  return (
-    <div className="flex w-full h-full justify-center pt-16">
-      <div className="w-11/12 m-4 sm:w-7/12 xl:w-1/3 md:m-0">
-        <p className="p-4 pb-12 text-xl font-bold text-center"> create your new event </p>
-        <CreateEventForm
-          createEvent={backend.createEvent}
+  const [openDialog, setOpenDialog] = useState(false)
+
+  const basePath = import.meta.env.BASE_URL
+
+  const navbar =
+    <NavbarWithSlots
+      left={<div>
+        <BackButton pathToLinkTo={basePath} />
+      </div>}
+      right={<div> </div>}
+    >
+      create event
+    </NavbarWithSlots>
+
+  const footer =
+    <FooterWithSlots
+      left={<div> </div>}
+      right={
+        <ConnectionStatusBar
+          status={globalContext.connectionStatus}
         />
-      </div>
+      }
+    >
+    </FooterWithSlots >
+
+  return (
+    <div>
+      <AppFrame top={navbar} bottom={footer}>
+        <div className="flex w-full h-full justify-center pt-16">
+          <div className="w-11/12 m-4 sm:w-7/12 xl:w-1/3 md:m-0">
+            <CreateEventForm
+              createEvent={backend.createEvent}
+            />
+          </div>
+        </div>
+      </AppFrame>
     </div>
   )
-
 }
 
 export { CreatePage }
